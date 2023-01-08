7a1669228dcf70fe3462497dbb19104d9e5e2825
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-08 15:07:25 +0900

    스프링 부트 Actuator 1부: 소개

---
+++ src/test/java/me/whiteship/springbootactuatordemo/SpringbootactuatordemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.springbootactuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootactuatordemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
